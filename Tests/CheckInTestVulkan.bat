1fa8e6d6667767273453d7a5feac06408d11aabb
ashwinNV <ashwinv@nvidia.com>
2017-08-16 09:56:03 -0700

    Hmm

--- Tests/CheckInTestVulkan.bat
+++ Tests/CheckInTestVulkan.bat
@@ -1,2 +1,2 @@
-REM Check In Test Vulkan - Runs the Release version of the Vulkan Tests, locally.
-call python.exe RunTestsSet.py -d ../ -sln Falcor.sln -cfg ReleaseVK -ts TestsCollectionsAndSets/TestsSetD3D12.json
+REM Check In Test D3D12 - Runs the Release version of the D3D12 Tests, locally.
+call python.exe RunTestsSet.py -nb -md ../ -ts TestsCollectionsAndSets\TSReleaseVK.json
