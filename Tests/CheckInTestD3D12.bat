27efd5e6645436265c7b4e4a08aae2e05d4d191a
ashwinNV <ashwinv@nvidia.com>
2017-08-16 05:38:55 -0700

    Removed manual ref

--- Tests/CheckInTestD3D12.bat
+++ Tests/CheckInTestD3D12.bat
@@ -1,2 +1,2 @@
 REM Check In Test D3D12 - Runs the Release version of the D3D12 Tests, locally.
-call python.exe RunTestsSet.py -nb -md ../ -ts TestsCollectionsAndSets\TSReleaseD3D12.json -ref \\netapp-wa02\public\ashwinv\References\ashwinv_dt\
+call python.exe RunTestsSet.py -md ../ -ts TestsCollectionsAndSets\TSReleaseD3D12.json
