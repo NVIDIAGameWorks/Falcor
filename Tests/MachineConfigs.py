18d7b56ce0b579c211dd83169d1a94fa1d3480cf
ashwinv <ashwinv@nvidia.com>
2017-08-16 23:03:12 -0700

    Change Machine Configs

--- Tests/MachineConfigs.py
+++ Tests/MachineConfigs.py
@@ -7,5 +7,5 @@ machine_build_script = "BuildSolution.bat"
 machine_process_default_kill_time = 1200.0
 
 machine_relative_checkin_local_results_directory = "TestsResults\\local-results-directory\\"
-machine_default_checkin_reference_directory = "\\\\netapp-wa02\\public\\Falcor\\GitHub\\References\\" + machine_name + '\\new-testing-system\\TC_EverydayTests\\'
+machine_default_checkin_reference_directory = "\\\\netapp-wa02\\public\\Falcor\\GitHub\\References\\" + machine_name + '\\new-testing-system\\EverydayTests\\'
 machine_email_recipients = "\\\\netapp-wa02\\public\\Falcor\\email.txt"
