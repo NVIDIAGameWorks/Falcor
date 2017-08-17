d61e94410c073649d8ca52e02d943a9796b5f699
ashwinNV <ashwinv@nvidia.com>
2017-08-16 18:37:58 -0700

    Merge master

--- Tests/MachineConfigs.py
+++ Tests/MachineConfigs.py
@@ -4,7 +4,7 @@ machine_name = os.environ['COMPUTERNAME']
 machine_name = machine_name.lower()
 
 machine_build_script = "BuildSolution.bat"
-machine_process_default_kill_time = 300.0
+machine_process_default_kill_time = 1200.0
 
 machine_relative_checkin_local_results_directory = "TestsResults\\local-results-directory\\"
 machine_default_checkin_reference_directory = "\\\\netapp-wa02\\public\\ashwinv\\GitHub\\References\\" + machine_name + '\\new-testing-system\\TC_EverydayTests\\'
