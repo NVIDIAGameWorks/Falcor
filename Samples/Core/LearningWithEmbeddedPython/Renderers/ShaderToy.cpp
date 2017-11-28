32f25c32150d7c7385a64bc8cc65f5153629673f
nbenty <nbenty@nvidia.com>
2017-11-27 16:56:50 -0800

    Fixed ShaderToy build. D3D12 test passes

--- Samples/Core/LearningWithEmbeddedPython/Renderers/ShaderToy.cpp
+++ Samples/Core/LearningWithEmbeddedPython/Renderers/ShaderToy.cpp
@@ -58,7 +58,7 @@ void ShaderToyRenderer::onInitialize(RenderContext::SharedPtr)
     mpToyVars = GraphicsVars::create(mpMainPass->getProgram()->getActiveVersion()->getReflector());
 
     // Get buffer finding
-    mToyCBBinding = mpMainPass->getProgram()->getActiveVersion()->getReflector()->getBufferBinding("ToyCB").regIndex;
+    mToyCBBinding = mpMainPass->getProgram()->getActiveVersion()->getReflector()->getResourceBinding("ToyCB").regIndex;
 
     // Create a text renderer
     mTextRender = TextRenderer::create();
