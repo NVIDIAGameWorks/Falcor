0ac37f6ac7dd0d247c963637a4e684eda046ff86
nbenty <nbenty@nvidia.com>
2017-12-12 16:33:26 -0800

    More comments

--- Framework/Source/Graphics/Program/ParameterBlock.h
+++ Framework/Source/Graphics/Program/ParameterBlock.h
@@ -36,6 +36,8 @@ namespace Falcor
     class RootSignature;
     class ProgramVars;
 
+    /** A parameter-block object. This object is stores all the resources and descriptor-sets required by a specific parameter-block in a program
+    */
     class ParameterBlock : public std::enable_shared_from_this<ParameterBlock>
     {
     public:
@@ -70,12 +72,12 @@ namespace Falcor
         bool setConstantBuffer(const std::string& name, const ConstantBuffer::SharedPtr& pCB);
 
         /** Bind a constant buffer object by index.
-        If the no CB exists in the specified index or the CB size doesn't match the required size, the call will fail.
-        If a buffer was previously bound it will be released.
-        \param[in] setIndex The set-index in the block
-        \param[in] descIndex The descriptor index inside the set
-        \param[in] pCB The constant buffer object
-        \return false is the call failed, otherwise true
+            If the no CB exists in the specified index or the CB size doesn't match the required size, the call will fail.
+            If a buffer was previously bound it will be released.
+            \param[in] bindLocation The bind-location in the block
+            \param[in] arrayIndex The array index, or 0 for non-arrays
+            \param[in] pCB The constant buffer object
+            \return false is the call failed, otherwise true
         */
         bool setConstantBuffer(const BindLocation& bindLocation, uint32_t arrayIndex, const ConstantBuffer::SharedPtr& pCB);
 
@@ -86,99 +88,99 @@ namespace Falcor
         ConstantBuffer::SharedPtr getConstantBuffer(const std::string& name) const;
 
         /** Get a constant buffer object.
-        \param[in] setIndex The set-index in the block
-        \param[in] descIndex The descriptor index inside the set
-        \return If the indices is valid, a shared pointer to the buffer. Otherwise returns nullptr
+            \param[in] bindLocation The bind-location in the block
+            \param[in] arrayIndex The array index, or 0 for non-arrays
+            \return If the indices is valid, a shared pointer to the buffer. Otherwise returns nullptr
         */
         ConstantBuffer::SharedPtr getConstantBuffer(const BindLocation& bindLocation, uint32_t arrayIndex) const;
 
         /** Set a raw-buffer. Based on the shader reflection, it will be bound as either an SRV or a UAV
-        \param[in] name The name of the buffer
-        \param[in] pBuf The buffer object
+            \param[in] name The name of the buffer
+            \param[in] pBuf The buffer object
         */
         bool setRawBuffer(const std::string& name, Buffer::SharedPtr pBuf);
 
         /** Set a typed buffer. Based on the shader reflection, it will be bound as either an SRV or a UAV
-        \param[in] name The name of the buffer
-        \param[in] pBuf The buffer object
+            \param[in] name The name of the buffer
+            \param[in] pBuf The buffer object
         */
         bool setTypedBuffer(const std::string& name, TypedBufferBase::SharedPtr pBuf);
         
         /** Set a structured buffer. Based on the shader reflection, it will be bound as either an SRV or a UAV
-        \param[in] name The name of the buffer
-        \param[in] pBuf The buffer object
+            \param[in] name The name of the buffer
+            \param[in] pBuf The buffer object
         */
         bool setStructuredBuffer(const std::string& name, StructuredBuffer::SharedPtr pBuf);
 
         /** Get a raw-buffer object.
-        \param[in] name The name of the buffer
-        \return If the name is valid, a shared pointer to the buffer object. Otherwise returns nullptr
+            \param[in] name The name of the buffer
+            \return If the name is valid, a shared pointer to the buffer object. Otherwise returns nullptr
         */
         Buffer::SharedPtr getRawBuffer(const std::string& name) const;
 
         /** Get a typed buffer object.
-        \param[in] name The name of the buffer
-        \return If the name is valid, a shared pointer to the buffer object. Otherwise returns nullptr
+            \param[in] name The name of the buffer
+            \return If the name is valid, a shared pointer to the buffer object. Otherwise returns nullptr
         */
         TypedBufferBase::SharedPtr getTypedBuffer(const std::string& name) const;
 
         /** Get a structured buffer object.
-        \param[in] name The name of the buffer
-        \return If the name is valid, a shared pointer to the buffer object. Otherwise returns nullptr
+            \param[in] name The name of the buffer
+            \return If the name is valid, a shared pointer to the buffer object. Otherwise returns nullptr
         */
         StructuredBuffer::SharedPtr getStructuredBuffer(const std::string& name) const;
 
         /** Bind a texture. Based on the shader reflection, it will be bound as either an SRV or a UAV
-        \param[in] name The name of the texture object in the shader
-        \param[in] pTexture The texture object to bind
+            \param[in] name The name of the texture object in the shader
+            \param[in] pTexture The texture object to bind
         */
         bool setTexture(const std::string& name, const Texture::SharedPtr& pTexture);
 
         /** Get a texture object.
-        \param[in] name The name of the texture
-        \return If the name is valid, a shared pointer to the texture object. Otherwise returns nullptr
+            \param[in] name The name of the texture
+            \return If the name is valid, a shared pointer to the texture object. Otherwise returns nullptr
         */
         Texture::SharedPtr getTexture(const std::string& name) const;
 
         /** Bind an SRV.
-        \param[in] setIndex The set-index in the block
-        \param[in] descIndex The descriptor index inside the set
-        \param[in] pSrv The shader-resource-view object to bind
+            \param[in] bindLocation The bind-location in the block
+            \param[in] arrayIndex The array index, or 0 for non-arrays
+            \param[in] pSrv The shader-resource-view object to bind
         */
         bool setSrv(const BindLocation& bindLocation, uint32_t arrayIndex, const ShaderResourceView::SharedPtr& pSrv);
 
         /** Bind a UAV.
-        \param[in] setIndex The set-index in the block
-        \param[in] descIndex The descriptor index inside the set
-        \param[in] pSrv The unordered-access-view object to bind
+            \param[in] bindLocation The bind-location in the block
+            \param[in] arrayIndex The array index, or 0 for non-arrays
+            \param[in] pSrv The unordered-access-view object to bind
         */
         bool setUav(const BindLocation& bindLocation, uint32_t arrayIndex, const UnorderedAccessView::SharedPtr& pUav);
 
         /** Get an SRV object.
-        \param[in] setIndex The set-index in the block
-        \param[in] descIndex The descriptor index inside the set
-        \return If the indices is valid, a shared pointer to the SRV. Otherwise returns nullptr
+            \param[in] bindLocation The bind-location in the block
+            \param[in] arrayIndex The array index, or 0 for non-arrays
+            \return If the indices is valid, a shared pointer to the SRV. Otherwise returns nullptr
         */
         ShaderResourceView::SharedPtr getSrv(const BindLocation& bindLocation, uint32_t arrayIndex) const;
 
         /** Get a UAV object
-        \param[in] setIndex The set-index in the block
-        \param[in] descIndex The descriptor index inside the set
-        \return If the index is valid, a shared pointer to the UAV. Otherwise returns nullptr
+            \param[in] bindLocation The bind-location in the block
+            \param[in] arrayIndex The array index, or 0 for non-arrays
+            \return If the index is valid, a shared pointer to the UAV. Otherwise returns nullptr
         */
         UnorderedAccessView::SharedPtr getUav(const BindLocation& bindLocation, uint32_t arrayIndex) const;
 
         /** Bind a sampler to the program in the global namespace.
-        \param[in] name The name of the sampler object in the shader
-        \param[in] pSampler The sampler object to bind
-        \return false if the sampler was not found in the program, otherwise true
+            \param[in] name The name of the sampler object in the shader
+            \param[in] pSampler The sampler object to bind
+            \return false if the sampler was not found in the program, otherwise true
         */
         bool setSampler(const std::string& name, const Sampler::SharedPtr& pSampler);
 
         /** Bind a sampler to the program in the global namespace.
-        \param[in] setIndex The set-index in the block
-        \param[in] descIndex The descriptor index inside the set
-        \return false if the sampler was not found in the program, otherwise true
+            \param[in] bindLocation The bind-location in the block
+            \param[in] arrayIndex The array index, or 0 for non-arrays
+            \return false if the sampler was not found in the program, otherwise true
         */
         bool setSampler(const BindLocation& bindLocation, uint32_t arrayIndex, const Sampler::SharedPtr& pSampler);
 
@@ -188,9 +190,9 @@ namespace Falcor
         Sampler::SharedPtr getSampler(const std::string& name) const;
 
         /** Gets a sampler object.
-        \param[in] setIndex The set-index in the block
-        \param[in] descIndex The descriptor index inside the set
-        \return If the index is valid, a shared pointer to the sampler. Otherwise returns nullptr
+            \param[in] bindLocation The bind-location in the block
+            \param[in] arrayIndex The array index, or 0 for non-arrays
+            \return If the index is valid, a shared pointer to the sampler. Otherwise returns nullptr
         */
         Sampler::SharedPtr getSampler(const BindLocation& bindLocation, uint32_t arrayIndex) const;
 
@@ -198,6 +200,8 @@ namespace Falcor
         */
         ParameterBlockReflection::SharedConstPtr getReflection() const { return mpReflector; }
 
+        /** Prepare the block for draw. This call updates the descriptor-sets
+        */
         bool prepareForDraw(CopyContext* pContext);
        
         // Delete some functions. If they are not deleted, the compiler will try to convert the uints to string, resulting in runtime error
@@ -207,11 +211,16 @@ namespace Falcor
         ConstantBuffer::SharedPtr getConstantBuffer(uint32_t) const = delete;
 
         // #PARAMBLOCK I don't like it. This should be private
+        /** Data structure describing a descriptor set. The dirty flag will tell us whether or not the set has changed since the last call to prepareForDraw()
+        */
         struct RootSet
         {
             DescriptorSet::SharedPtr pSet;
             bool dirty = true;
         };
+
+        /** Get the root-sets
+        */
         std::vector<RootSet>& getRootSets() { return mRootSets; }
     private:
         ParameterBlock(const ParameterBlockReflection::SharedConstPtr& pReflection, bool createBuffers);
