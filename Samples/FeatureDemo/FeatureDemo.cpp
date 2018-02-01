072b85fabb30aa2c015ea22bace16d17a0655d70
Kai-Hwa Yao <kyao@nvidia.com>
2018-01-31 17:48:24 -0800

    Defaulted Feature Demo tone mapping to ACES

--- Samples/FeatureDemo/FeatureDemo.cpp
+++ Samples/FeatureDemo/FeatureDemo.cpp
@@ -243,7 +243,7 @@ void FeatureDemo::initTAA()
 
 void FeatureDemo::initPostProcess()
 {
-    mpToneMapper = ToneMapping::create(ToneMapping::Operator::HableUc2);
+    mpToneMapper = ToneMapping::create(ToneMapping::Operator::Aces);
 }
 
 void FeatureDemo::onLoad()
