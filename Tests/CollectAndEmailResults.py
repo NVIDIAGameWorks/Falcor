8e0ba05fd25811cc94068bfa631852d0d4e9952f
Conor Lavelle <clavelle@nvidia.com>
2018-02-28 12:40:36 -0800

    Moved CollectAndEmailResults.py code out of function since it's meant to be invoked directly
    old implementation implied it was used as a module in other scripts and it wasn't

--- Tests/CollectAndEmailResults.py
+++ Tests/CollectAndEmailResults.py
@@ -3,32 +3,29 @@ import Helpers as helpers
 import os
 from datetime import date
 
-def collect_and_email_results():
-    target_base = machine_configs.machine_results_summary_target
-    date_str = date.today().strftime("%m-%d-%y")
-    today_dir = os.path.join(target_base, date_str)    
+target_base = machine_configs.machine_results_summary_target
+date_str = date.today().strftime("%m-%d-%y")
+today_dir = os.path.join(target_base, date_str)    
+
+files = []
+for file in os.listdir(today_dir):
+    if file.endswith('.html'):
+        files.append(os.path.join(today_dir, file))
     
-    files = []
-    for file in os.listdir(today_dir):
-        if file.endswith('.html'):
-            files.append(os.path.join(today_dir, file))
-        
-    num_files = len(files)
-    num_success = 0
-    for file in files:
-        if file.find('[SUCCESS]') != -1:
-            num_success += 1
-    
-    subject = ''
-    if num_files == num_success:
-        subject += '[SUCCESS (' 
-    else:
-        subject += '[FAILURE (' 
-    
-    subject += str(num_success) + ' / ' + str(num_files) + ')]'
-    subject += '_' + date_str
-    helpers.dispatch_email(subject, files)
-    
-if __name__ == '__main__':
-    collect_and_email_results()
+num_files = len(files)
+num_success = 0
+for file in files:
+    if file.find('[SUCCESS]') != -1:
+        num_success += 1
+
+subject = ''
+if num_files == num_success:
+    subject += '[SUCCESS (' 
+else:
+    subject += '[FAILURE (' 
+
+subject += str(num_success) + ' / ' + str(num_files) + ')]'
+subject += '_' + date_str
+helpers.dispatch_email(subject, files)
+
     
