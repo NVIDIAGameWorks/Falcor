85ef523caf791371e47f042cef78404ffe1494c0
Kai-Hwa Yao <kyao@nvidia.com>
2018-04-20 15:18:52 -0700

    Removed enabling VK_LAYER_NV_nsight layer

--- Framework/Source/API/Vulkan/VKDevice.cpp
+++ Framework/Source/API/Vulkan/VKDevice.cpp
@@ -269,7 +269,6 @@ namespace Falcor
         if (desc.enableDebugLayer)
         {
             enableLayerIfPresent("VK_LAYER_LUNARG_standard_validation", layerProperties, requiredLayers);
-            enableLayerIfPresent("VK_LAYER_NV_nsight", layerProperties, requiredLayers);
         }
 
         // Initialize the extensions
