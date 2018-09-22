a5a7d353130d87628a71b2f9bbe2d705a99b9ba8
moakes <moakes@nvidia.com>
2018-09-21 17:11:02 -0700

    Minor warnings fixes

--- Framework/Source/Utils/PatternGenerators/PatternGenerator.h
+++ Framework/Source/Utils/PatternGenerators/PatternGenerator.h
@@ -33,7 +33,7 @@ namespace Falcor
     {
     public:
         using SharedPtr = std::shared_ptr<PatternGenerator>;
-        virtual ~PatternGenerator() = 0 {}
+        virtual ~PatternGenerator() {}
 
         virtual uint32_t getSampleCount() const = 0;
         virtual void reset(uint32_t startID = 0) = 0;
