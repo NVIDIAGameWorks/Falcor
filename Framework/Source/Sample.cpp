9ca4f383f640f760a2b16a36c635d436c277ca15
vincentz <vincentz@nvidia.com>
2018-09-29 15:37:45 +0800

    Check nullness of gpDevice in Sample::runInternal()

--- Framework/Source/Sample.cpp
+++ Framework/Source/Sample.cpp
@@ -274,7 +274,10 @@ namespace Falcor
         mpWindow->msgLoop();
 
         mpRenderer->onShutdown(this);
-        gpDevice->flushAndSync();
+        if (gpDevice)
+        {
+            gpDevice->flushAndSync();
+        }
         mpRenderer = nullptr;
         Logger::shutdown();
     }
