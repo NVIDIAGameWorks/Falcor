ad87cc590aa96c5db312da63722ff840ad7f7831
skallweitNV <64953474+skallweitNV@users.noreply.github.com>
2023-10-23 17:16:59 +0200

    Add WAR diff path tracer script (#396)

---
+++ scripts/WARDiffPathTracer.py
@@ -0,0 +1,36 @@
+from falcor import *
+
+def render_graph_WARDiffPathTracer():
+    g = RenderGraph("WARDiffPathTracer")
+    WARDiffPathTracer = createPass("WARDiffPathTracer",
+                                   {
+                                       "maxBounces": 0,
+                                       "samplesPerPixel": 1,
+                                       "diffMode": "ForwardDiffDebug",
+                                       #"diffVarName": "CBOX_BUNNY_MATERIAL",
+                                       "diffVarName": "CBOX_BUNNY_TRANSLATION",
+                                   })
+    g.addPass(WARDiffPathTracer, "WARDiffPathTracer")
+
+    AccumulatePassPrimal = createPass("AccumulatePass", {"enabled": True, "precisionMode": "Single"})
+    g.addPass(AccumulatePassPrimal, "AccumulatePassPrimal")
+
+    AccumulatePassDiff = createPass("AccumulatePass", {"enabled": True, 'precisionMode': "Single"})
+    g.addPass(AccumulatePassDiff, "AccumulatePassDiff")
+    ColorMapPassDiff = createPass("ColorMapPass", {"minValue": -4.0, "maxValue": 4.0, "autoRange": False})
+    g.addPass(ColorMapPassDiff, "ColorMapPassDiff")
+
+    g.addEdge("WARDiffPathTracer.color", "AccumulatePassPrimal.input")
+    g.addEdge("WARDiffPathTracer.dColor", "AccumulatePassDiff.input")
+    g.addEdge("AccumulatePassDiff.output", "ColorMapPassDiff.input")
+    g.markOutput("AccumulatePassDiff.output")
+    g.markOutput("AccumulatePassPrimal.output")
+    g.markOutput("ColorMapPassDiff.output")
+    return g
+
+WARDiffPathTracer = render_graph_WARDiffPathTracer()
+try: m.addGraph(WARDiffPathTracer)
+except NameError: None
+
+flags = SceneBuilderFlags.DontMergeMaterials | SceneBuilderFlags.RTDontMergeDynamic | SceneBuilderFlags.DontOptimizeMaterials
+m.loadScene("test_scenes/bunny_war_diff_pt.pyscene", buildFlags=flags)
